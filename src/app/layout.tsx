1483b8f2ec69d2c5a4c633684eb55b9f19925976
imadatyatalah <imadatyatalah@gmail.com>
2023-07-23 15:35:37 +0100

    feat: add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Footer from "@/components/footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -52,6 +53,8 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         {children}
 
         <Footer />
+
+        <Analytics />
       </body>
     </html>
   );
