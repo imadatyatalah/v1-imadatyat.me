8d47fe9ac13ab55d2ad193fbf11054c6c0f34852
imadatyatalah <imadatyatalah@gmail.com>
2023-08-11 16:58:55 +0100

    feat: add Experience section

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["media.licdn.com"],
+  },
   headers() {
     return [
       {
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,54 @@
 import { Metadata } from "next";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "About",
+};
+
+const ExperienceCard = ({
+  experience,
+}: {
+  experience: {
+    logo: string;
+    jobTitle: string;
+    company: string;
+    employmentType: string;
+    hiringDate: string;
+    content?: React.ReactNode;
+  };
+}) => {
+  const { logo, jobTitle, company, employmentType, hiringDate, content } =
+    experience;
+
+  return (
+    <div className="flex space-x-3">
+      <div>
+        <div className="relative w-[3.125rem] h-[3.125rem]">
+          <Image
+            src={logo}
+            width={50}
+            height={50}
+            alt="Obytes' logo"
+            className="rounded-full"
+          />
+        </div>
+      </div>
+
+      <div className="space-y-2">
+        <div>
+          <h3 className="text-lg font-semibold">{jobTitle}</h3>
+
+          <h4 className="text-sm">
+            {company} - {employmentType}
+          </h4>
+
+          <p className="text-sm text-gray-400">{hiringDate}</p>
+        </div>
+
+        {content}
+      </div>
+    </div>
+  );
 };
 
 const AboutPage = () => {
@@ -24,6 +71,67 @@ const AboutPage = () => {
               to continually enhancing my skills and expertise in the field of
               web development.
             </p>
+          </div>
+        </div>
+
+        <div className="space-y-4">
+          <h2 className="text-2xl font-bold">Experience</h2>
+
+          <div className="space-y-4">
+            <ExperienceCard
+              experience={{
+                logo: "https://media.licdn.com/dms/image/C4E0BAQG-0DgOT34Vqg/company-logo_100_100/0/1673637608325?e=1699488000&v=beta&t=et20J6QM4KDtj4JGy7M-0OY8a78K6P_04ZJEh-xcpdg",
+                jobTitle: "Co-Founder",
+                company: "Portfolly",
+                employmentType: "Full-time",
+                hiringDate: "Sep 2022 - Present",
+              }}
+            />
+
+            <ExperienceCard
+              experience={{
+                logo: "https://media.licdn.com/dms/image/D4E0BAQF7JmbdIyVoTg/company-logo_100_100/0/1666750453797?e=1700092800&v=beta&t=uA9l3ewDYZ6MNfcOx6SfpXWNNqewsD6yxjBrrZNgg18",
+                jobTitle: "Front-end Lead (via Obytes)",
+                company: "Lineup",
+                employmentType: "Full-time",
+                hiringDate: "Aug 2022 - Present",
+                content: (
+                  <div className="text-sm">
+                    <ul className="list-disc list-inside">
+                      <li>
+                        Led the front-end team, making sure we built a
+                        great-looking and smooth-working web app.
+                      </li>
+
+                      <li>
+                        Handled the setup of Stripe payments and Stripe Connect,
+                        so customers could easily and safely pay online.
+                      </li>
+
+                      <li>
+                        Figured out how to add tricky new stuff to the site and
+                        made sure it all worked well.
+                      </li>
+
+                      <li>
+                        Checked out my teammates&apos; code on GitHub to help
+                        catch any issues and make sure everything was on track.
+                      </li>
+                    </ul>
+                  </div>
+                ),
+              }}
+            />
+
+            <ExperienceCard
+              experience={{
+                logo: "https://media.licdn.com/dms/image/C4D0BAQHl_VZFKwbItw/company-logo_100_100/0/1659460397480?e=1699488000&v=beta&t=-aBySiXHHS_afwdhCQ17_GEC4rETyajdVklHMsj-ntM",
+                jobTitle: "Front-end Developer",
+                company: "Obytes",
+                employmentType: "Full-time",
+                hiringDate: "Apr 2022 - Present",
+              }}
+            />
           </div>
         </div>
 
@@ -68,12 +176,6 @@ const AboutPage = () => {
             </ul>
           </div>
         </div>
-
-        {/* <div className="space-y-4">
-          <h2 className="text-2xl font-bold">Experience</h2>
-
-          <div>WIP</div>
-        </div> */}
       </section>
     </main>
   );
